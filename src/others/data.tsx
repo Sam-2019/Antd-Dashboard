9973ed76fb9c418f5823fc20b35b432366c71c52
Sam-2019 <samtmartey@gmail.com>
2021-08-22 17:43:35 +0100

    added data

---
+++ src/others/data.tsx
@@ -0,0 +1,144 @@
+import {
+  DashboardOutlined,
+  TeamOutlined,
+  GroupOutlined,
+} from "@ant-design/icons";
+
+export const month = [
+  {
+    key: "1",
+    name: "January",
+    path: "/january",
+  },
+  {
+    key: "2",
+    name: "February",
+    path: "/february",
+  },
+  {
+    key: "3",
+    name: "March",
+    path: "/march",
+  },
+  {
+    key: "4",
+    name: "April",
+    path: "/april",
+  },
+  {
+    key: "5",
+    name: "May",
+    path: "/may",
+  },
+  {
+    key: "6",
+    name: "June",
+    path: "/june",
+  },
+  {
+    key: "7",
+    name: "July",
+    path: "/july",
+  },
+  {
+    key: "8",
+    name: "August",
+    path: "/august",
+  },
+  {
+    key: "9",
+    name: "September",
+    path: "/september",
+  },
+  {
+    key: "10",
+    name: "October",
+    path: "/october",
+  },
+  {
+    key: "11",
+    name: "November",
+    path: "/november",
+  },
+  {
+    key: "12",
+    name: "December",
+    path: "/december",
+  },
+];
+
+export const menuItems = [
+  {
+    key: "1",
+    name: "Dashboard",
+    path: "/",
+    icon: <DashboardOutlined />,
+  },
+  {
+    key: "2",
+    name: "Departments",
+    path: "/departments",
+    icon: <GroupOutlined />,
+  },
+  {
+    key: "3",
+    name: "Members",
+    path: "/members",
+    icon: <TeamOutlined />,
+  },
+  {
+    key: "4",
+    name: "Tithe",
+    path: "/tithe",
+    icon: <TeamOutlined />,
+  },
+  {
+    key: "5",
+    name: "Chapels",
+    path: "/chapels",
+    icon: <TeamOutlined />,
+  },
+];
+
+export const departments = [
+  {
+    key: "1",
+    name: "Admininstration",
+    path: "/admin",
+  },
+  {
+    key: "2",
+    name: "Protocol",
+    path: "/protocol",
+  },
+  {
+    key: "3",
+    name: "Audi-Visual",
+    path: "/audio-visual",
+  },
+  {
+    key: "4",
+    name: "Traffic Control",
+    path: "/traffic-control",
+  },
+  {
+    key: "5",
+    name: "Choir",
+    path: "/choir",
+  },
+  {
+    key: "6",
+    name: "Omega",
+    path: "/omega",
+  },
+  {
+    key: "7",
+    name: "Deacon",
+    path: "/deacon",
+  },
+  {
+    key: "8",
+    name: "Children Service",
+    path: "/children-service",
+  },
+];
