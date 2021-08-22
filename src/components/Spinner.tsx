a76cee2d2a50f930016060ebdbe652941ed3491d
Sam-2019 <samtmartey@gmail.com>
2021-08-22 20:26:05 +0100

    added Spinner

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,13 @@
+import { Spin } from 'antd';
+
+import React from 'react'
+
+function Spinner() {
+    return (
+        <div className="example">
+        <Spin />
+      </div>
+    )
+}
+
+export default Spinner
