a3f8f3b41eddda37a521290f824570cfbbb938fa
Sam-2019 <samtmartey@gmail.com>
2021-08-22 19:55:45 +0100

    updated GoBack component

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,14 +1,15 @@
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { PageHeader } from "antd";
 
 const GoBack = () => {
   const history = useHistory();
+  let { slug }: any = useParams();
 
   return (
     <PageHeader
       className="site-page-header goBack"
       onBack={() => history.goBack()}
-      title="Back"
+      title={<span className="breadcrumb">{slug}</span>}
     />
   );
 };
