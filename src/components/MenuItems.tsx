398d225c6fe0f020c0bb28dfc446978cc599e7d9
Sam-2019 <samtmartey@gmail.com>
2021-08-23 20:07:43 +0100

    removed div

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -7,19 +7,17 @@ const MenuItems = () => {
   const history = useHistory();
 
   return (
-    <div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-        {menuItems.map((data: any) => (
-          <Menu.Item
-            key={data.key}
-            icon={data.icon}
-            onClick={() => history.push(data.path)}
-          >
-            {data.name}
-          </Menu.Item>
-        ))}
-      </Menu>
-    </div>
+    <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+      {menuItems.map((data: any) => (
+        <Menu.Item
+          key={data.key}
+          icon={data.icon}
+          onClick={() => history.push(data.path)}
+        >
+          {data.name}
+        </Menu.Item>
+      ))}
+    </Menu>
   );
 };
 
