f843dfb0ebe9734bcb068ec60eccf408b0de6b35
Sam-2019 <samtmartey@gmail.com>
2021-08-23 20:22:32 +0100

    imported List

--- src/pages/Department/Department.tsx
+++ src/pages/Department/Department.tsx
@@ -1,83 +1,12 @@
 import React from "react";
-import { Table, Tag, Space } from "antd";
-import Column from "antd/lib/table/Column";
-import { Link } from "react-router-dom";
-import GoBack from '../../components/GoBack'
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"],
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"],
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    tags: ["cool", "teacher"],
-  },
-];
+import { useParams } from "react-router-dom";
+import List from "../../components/List";
+import Spinner from "../../components/Spinner";
 
 function Department() {
-  return (
-    <div>
-      <GoBack />
-      <Table dataSource={data}>
-        <Column
-          title="Name"
-          dataIndex="name"
-          key="name"
-          render={(text: string): any => (
-            <Space size="middle">
-              <Link to={`/members/${text}`}>{text}</Link>
-            </Space>
-          )}
-        />
-        <Column title="Age" dataIndex="age" key="age" />
-        <Column title="Address" dataIndex="address" key="address" />
-        <Column
-          title="Tags"
-          dataIndex="tags"
-          key="tags"
-          render={(tags) => (
-            <>
-              {tags.map((tag: string): any => {
-                let color = tag.length > 5 ? "geekblue" : "green";
-                if (tag === "loser") {
-                  color = "volcano";
-                }
-                return (
-                  <Tag color={color} key={tag}>
-                    {tag.toUpperCase()}
-                  </Tag>
-                );
-              })}
-            </>
-          )}
-        />
-        {/* <Column
-        title="Action"
-        dataIndex="action"
-        key="action"
-        render={(text, record) => (
-          <Space size="middle">
-            <a>Invite {record.lastName}</a>
-            <a>Delete</a>
-          </Space>
-        )}
-      /> */}
-      </Table>
-    </div>
-  );
+  let { slug }: any = useParams();
+
+  return <div>{slug ? <List slug={slug} /> : <Spinner />}</div>;
 }
 
 export default Department;
