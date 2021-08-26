3d4cac1e577cce0f030205cebb86bee4309e195b
Sam-2019 <samtmartey@gmail.com>
2021-08-26 22:11:32 +0100

    updated form for mobile view

--- src/components/Forms/Sunday.tsx
+++ src/components/Forms/Sunday.tsx
@@ -1,187 +1,216 @@
-import {
-  Input,
-  Col,
-  Row,
-  InputNumber,
-  DatePicker,
-  Form,
-  TimePicker,
-  Button,
-} from "antd";
+import { Input, InputNumber, DatePicker, Form, TimePicker, Button } from "antd";
 import GoBack from "../GoBack";
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 8 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 16 },
-  },
-};
 
 function Sunday() {
   const onFinish = (fieldsValue: any) => {
-    // Should format date value before submit.
-
     const values = {
       ...fieldsValue,
       "date-picker": fieldsValue["date-picker"].format("YYYY-MM-DD"),
-
       "time-picker": fieldsValue["time-picker"].format("HH:mm:ss"),
+      "time-picker2": fieldsValue["time-picker2"].format("HH:mm:ss"),
     };
     console.log("Received values of form: ", values);
   };
   return (
-    <div>
+    <>
       <GoBack />
-      <Form
-        name="time_related_controls"
-        {...formItemLayout}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="date-picker"
-          label="Date"
-          rules={[{ required: true, message: "Please select Date!" }]}
+      <div style={{}}>
+        <Form
+          name="sinday-form"
+          onFinish={onFinish}
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
         >
-          <DatePicker style={{ width: "36%" }} />
-        </Form.Item>
-
-        <Form.Item
-          name="time-picker"
-          label=" Start Time"
-          rules={[{ required: true, message: "Please select start time!" }]}
-        >
-          <TimePicker style={{ width: "36%" }}  />
-        </Form.Item>
-
-        <Form.Item
-          name="preacher"
-          label="Preacher"
-          rules={[{ required: true, message: "Preacher is required!" }]}
-        >
-          <Input style={{ width: "36%" }} />
-        </Form.Item>
-
-        <Form.Item
-          name="theme"
-          label="Theme"
-          rules={[{ required: true, message: "Theme is required!" }]}
-        >
-          <Input style={{ width: "36%" }} />
-        </Form.Item>
-
-        <Form.Item
-          name="bible-text"
-          label="Bible Text"
-          rules={[{ required: true, message: "Bible Text is required!" }]}
-          style={{ marginBottom: 10 }}
-        >
-          <Input style={{ width: "36%" }} />
-        </Form.Item>
-
-        <Form.Item
-          wrapperCol={{
-            xs: { span: 24, offset: 0 },
-            sm: { span: 13, offset: 8 },
-          }}
-          style={{ marginBottom: 10 }}
-        >
-          <Row>
-            <Col span={6}>Male</Col>
-
-            <Col span={6}>Female</Col>
-          </Row>
-        </Form.Item>
-
-        <Form.Item label="Adults" style={{ marginBottom: 0 }}>
           <Form.Item
-            name="adult-male"
-            style={{ display: "inline-block", width: "calc(20% - 8px)" }}
+            name="date-picker"
+            label="Date"
             rules={[{ required: true, message: "Required!" }]}
           >
-            <InputNumber keyboard={true} />
+            <DatePicker style={{ width: 200 }} />
           </Form.Item>
 
           <Form.Item
-            name="adult-female"
-            style={{
-              display: "inline-block",
-              width: "calc(20% - 8px)",
-              margin: "0 8px",
-            }}
+            name="time-picker"
+            label=" Start Time"
             rules={[{ required: true, message: "Required!" }]}
           >
-            <InputNumber keyboard={true} />
-          </Form.Item>
-        </Form.Item>
-
-        <Form.Item label="Omega Generation" style={{ marginBottom: 0 }}>
-          <Form.Item
-            name="omega-male"
-            style={{ display: "inline-block", width: "calc(20% - 8px)" }}
-            rules={[{ required: true, message: "Required!" }]}
-          >
-            <InputNumber keyboard={true} />
+            <TimePicker style={{ width: 200 }} />
           </Form.Item>
 
           <Form.Item
-            name="omega-female"
-            style={{
-              display: "inline-block",
-              width: "calc(20% - 8px)",
-              margin: "0 8px",
-            }}
+            name="preacher"
+            label="Preacher"
             rules={[{ required: true, message: "Required!" }]}
           >
-            <InputNumber keyboard={true} />
-          </Form.Item>
-        </Form.Item>
-
-        <Form.Item label="Children" style={{ marginBottom: 0 }}>
-          <Form.Item
-            name="children-male"
-            style={{ display: "inline-block", width: "calc(20% - 8px)" }}
-            rules={[{ required: true, message: "Required!" }]}
-          >
-            <InputNumber keyboard={true} />
+            <Input style={{ width: 200 }} />
           </Form.Item>
 
           <Form.Item
-            name="children-female"
-            style={{
-              display: "inline-block",
-              width: "calc(20% - 8px)",
-              margin: "0 8px",
-            }}
+            name="theme"
+            label="Theme"
             rules={[{ required: true, message: "Required!" }]}
           >
-            <InputNumber keyboard={true} />
+            <Input style={{ width: 200 }} />
           </Form.Item>
-        </Form.Item>
 
-        <Form.Item
-          name="time-picker"
-          label="End Time"
-          rules={[{ required: true, message: "Please select End Time!" }]}
-        >
-          <TimePicker style={{ width: "36%" }}  />
-        </Form.Item>
+          <Form.Item
+            name="bible-text"
+            label="Bible Text"
+            rules={[{ required: true, message: "Required!" }]}
+          >
+            <Input style={{ width: 200 }} />
+          </Form.Item>
 
-        <Form.Item
-          wrapperCol={{
-            xs: { span: 24, offset: 0 },
-            sm: { span: 16, offset: 8 },
-          }}
-        >
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+          <Form.Item label="Adult" style={{ marginBottom: 0 }}>
+            <Form.Item
+              name="adult-male"
+              rules={[{ required: true, message: "Required!" }]}
+              style={{
+                display: "inline-block",
+                width: "auto",
+                marginRight: 10,
+              }}
+            >
+              <InputNumber placeholder="Male" />
+            </Form.Item>
+            <Form.Item
+              name="adult-female"
+              rules={[{ required: true, message: "Required!" }]}
+              style={{ display: "inline-block", width: "auto" }}
+            >
+              <InputNumber placeholder="Female" />
+            </Form.Item>
+          </Form.Item>
+
+          <Form.Item label="Omega" style={{ marginBottom: 0 }}>
+            <Form.Item
+              name="omega-male"
+              rules={[{ required: true, message: "Required!" }]}
+              style={{
+                display: "inline-block",
+                width: "auto",
+                marginRight: 10,
+              }}
+            >
+              <InputNumber placeholder="Male" />
+            </Form.Item>
+            <Form.Item
+              name="omega-female"
+              rules={[{ required: true, message: "Required!" }]}
+              style={{ display: "inline-block", width: "auto" }}
+            >
+              <InputNumber placeholder="Female" />
+            </Form.Item>
+          </Form.Item>
+
+          <Form.Item label="Children" style={{ marginBottom: 0 }}>
+            <Form.Item
+              name="children-boy"
+              rules={[{ required: true, message: "Required!" }]}
+              style={{
+                display: "inline-block",
+                width: "auto",
+                marginRight: 10,
+              }}
+            >
+              <InputNumber placeholder="Male" />
+            </Form.Item>
+            <Form.Item
+              name="children-girl"
+              rules={[{ required: true, message: "Required!" }]}
+              style={{ display: "inline-block", width: "auto" }}
+            >
+              <InputNumber placeholder="Female" />
+            </Form.Item>
+          </Form.Item>
+
+          <Form.Item label="Alter Call" style={{ marginBottom: 0 }}>
+            <Form.Item
+              name="altercall-men"
+              style={{
+                display: "inline-block",
+                width: "auto",
+                marginRight: 10,
+              }}
+            >
+              <InputNumber placeholder="Male" />
+            </Form.Item>
+            <Form.Item
+              name="altercall-female"
+              style={{ display: "inline-block", width: "auto" }}
+            >
+              <InputNumber placeholder="Female" />
+            </Form.Item>
+          </Form.Item>
+
+          <Form.Item label="Visitors" style={{ marginBottom: 0 }}>
+            <Form.Item
+              name="visitors-men"
+              style={{
+                display: "inline-block",
+                width: "auto",
+                marginRight: 10,
+              }}
+            >
+              <InputNumber placeholder="Male" />
+            </Form.Item>
+            <Form.Item
+              name="visitors-female"
+              style={{ display: "inline-block", width: "auto" }}
+            >
+              <InputNumber placeholder="Female" />
+            </Form.Item>
+          </Form.Item>
+
+          <Form.Item label="Vehicles" style={{ marginBottom: 0 }}>
+            <Form.Item
+              name="cars"
+              rules={[{ required: true, message: "Required!" }]}
+              style={{
+                display: "inline-block",
+                width: "auto",
+                marginRight: 10,
+              }}
+            >
+              <InputNumber placeholder="Cars" />
+            </Form.Item>
+
+            <Form.Item
+              name="motors"
+              rules={[{ required: true, message: "Required!" }]}
+              style={{
+                display: "inline-block",
+                width: "auto",
+                marginRight: 10,
+              }}
+            >
+              <InputNumber placeholder="Motors" />
+            </Form.Item>
+            <Form.Item
+              name="bicycles"
+              rules={[{ required: true, message: "Required!" }]}
+              style={{ display: "inline-block", width: "auto" }}
+            >
+              <InputNumber placeholder="Bicycles" />
+            </Form.Item>
+          </Form.Item>
+
+          <Form.Item
+            name="time-picker2"
+            label=" Close Time"
+            rules={[{ required: true, message: "Required!" }]}
+          >
+            <TimePicker style={{ width: 200 }} />
+          </Form.Item>
+
+          <Form.Item label=" " colon={false}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
   );
 }
 
