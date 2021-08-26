d98633ab6986ae7fb7967a7abaef5f4efeb95443
Sam-2019 <samtmartey@gmail.com>
2021-08-26 22:48:26 +0100

    added useLocation to update page route

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,33 @@
 import { Card } from "antd";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 const { Meta } = Card;
 
 function CardView({ data }: any) {
   const history = useHistory();
+  let location = useLocation();
+
+  let pathSnippets = location.pathname.split("/").filter((i) => i);
+
   return (
     <div className="gridStyle">
       {data.map((data: any) => (
-        <div key={data.key} onClick={() => history.push(`/forms${data.path}`)}>
+        <div
+          key={data.key}
+          onClick={() => history.push(`/${pathSnippets[0]}${data.path}`)}
+        >
           <Card
             hoverable
             cover={
               <img
-                width={250}
+                width="auto"
                 height="auto"
                 alt="example"
                 src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
               />
             }
           >
-            <Meta title={data.name} />
+            <Meta description={data.name} />
           </Card>
         </div>
       ))}
