10afdf58dcc5d2f99e06bd07c8be97805ae777b3
Sam-2019 <samtmartey@gmail.com>
2021-08-27 10:03:51 +0100

    removed List

--- src/routes/Chapels.tsx
+++ src/routes/Chapels.tsx
@@ -1,14 +1,14 @@
 import { Switch, Route, useRouteMatch } from "react-router-dom";
-import Chapel from "../pages/Chapel/List";
 import Chapels from "../pages/Chapel";
+// import Chapel from "../pages/Chapel/List";
 
 export function ChapelsRoute() {
   let { path } = useRouteMatch();
   return (
     <Switch>
-      <Route path={`${path}/:slug`}>
+      {/* <Route path={`${path}/:slug`}>
         <Chapel />
-      </Route>
+      </Route> */}
 
       <Route path={`${path}`}>
         <Chapels />
