ee7b6f0015cd664526721f6503c33828ecf21cf3
Sam-2019 <samtmartey@gmail.com>
2021-08-27 10:13:15 +0100

    added ChapelLiast

--- src/routes/Chapels.tsx
+++ src/routes/Chapels.tsx
@@ -1,14 +1,14 @@
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import Chapels from "../pages/Chapel";
-// import Chapel from "../pages/Chapel/List";
+import Chapel from "../pages/Chapel/ChapelList";
 
 export function ChapelsRoute() {
   let { path } = useRouteMatch();
   return (
     <Switch>
-      {/* <Route path={`${path}/:slug`}>
+      <Route path={`${path}/:slug`}>
         <Chapel />
-      </Route> */}
+      </Route>
 
       <Route path={`${path}`}>
         <Chapels />
