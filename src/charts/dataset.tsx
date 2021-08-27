08966a0539c45948cc25421f327d2e88b0344fad
Sam-2019 <samtmartey@gmail.com>
2021-08-27 09:49:36 +0100

    added chart data

---
+++ src/charts/dataset.tsx
@@ -0,0 +1,206 @@
+export const total = [
+  {
+    ageGroup: "Adult",
+    date: "2021-07-11",
+    number: 18,
+  },
+  {
+    ageGroup: "Adult",
+    date: "2021-07-18",
+    number: 28,
+  },
+  {
+    ageGroup: "Adult",
+    date: "2021-07-25",
+    number: 39,
+  },
+  {
+    ageGroup: "Adult",
+    date: "2021-08-01",
+    number: 81,
+  },
+  {
+    ageGroup: "Adult",
+    date: "2021-08-08",
+    number: 47,
+  },
+  {
+    ageGroup: "Adult",
+    date: "2021-08-15",
+    number: 20,
+  },
+  {
+    ageGroup: "Adult",
+    date: "2021-08-22",
+    number: 24,
+  },
+  {
+    ageGroup: "Adult",
+    date: "2021-08-29",
+    number: 35,
+  },
+  {
+    ageGroup: "Omega",
+    date: "2021-07-11",
+    number: 12,
+  },
+  {
+    ageGroup: "Omega",
+    date: "2021-07-18",
+    number: 23,
+  },
+  {
+    ageGroup: "Omega",
+    date: "2021-07-25",
+    number: 34,
+  },
+  {
+    ageGroup: "Omega",
+    date: "2021-08-01",
+    number: 99,
+  },
+  {
+    ageGroup: "Omega",
+    date: "2021-08-08",
+    number: 52,
+  },
+  {
+    ageGroup: "Omega",
+    date: "2021-08-15",
+    number: 35,
+  },
+  {
+    ageGroup: "Omega",
+    date: "2021-08-22",
+    number: 37,
+  },
+  {
+    ageGroup: "Omega",
+    date: "2021-08-29",
+    number: 42,
+  },
+  {
+    ageGroup: "Children",
+    date: "2021-07-11",
+    number: 12,
+  },
+  {
+    ageGroup: "Children",
+    date: "2021-07-18",
+    number: 23,
+  },
+  {
+    ageGroup: "Children",
+    date: "2021-07-25",
+    number: 34,
+  },
+  {
+    ageGroup: "Children",
+    date: "2021-08-01",
+    number: 99,
+  },
+  {
+    ageGroup: "Children",
+    date: "2021-08-08",
+    number: 52,
+  },
+  {
+    ageGroup: "Children",
+    date: "2021-08-15",
+    number: 35,
+  },
+  {
+    ageGroup: "Children",
+    date: "2021-08-22",
+    number: 37,
+  },
+  {
+    ageGroup: "Children",
+    date: "2021-08-29",
+    number: 42,
+  },
+];
+
+export const stack = [
+  {
+    date: "2021-07-11",
+    value: 3,
+    type: "Female",
+  },
+  {
+    date: "2021-07-18",
+    value: 4,
+    type: "Female",
+  },
+  {
+    date: "2021-07-25",
+    value: 3,
+    type: "Female",
+  },
+  {
+    date: "2021-08-01",
+    value: 5,
+    type: "Female",
+  },
+  {
+    date: "2021-08-08",
+    value: 4,
+    type: "Female",
+  },
+  {
+    date: "2021-08-15",
+    value: 6,
+    type: "Female",
+  },
+  {
+    date: "2021-08-22",
+    value: 7,
+    type: "Female",
+  },
+  {
+    date: "2021-08-29",
+    value: 9,
+    type: "Female",
+  },
+
+  {
+    date: "2021-07-11",
+    value: 3,
+    type: "Male",
+  },
+  {
+    date: "2021-07-18",
+    value: 4,
+    type: "Male",
+  },
+  {
+    date: "2021-07-25",
+    value: 3,
+    type: "Male",
+  },
+  {
+    date: "2021-08-01",
+    value: 5,
+    type: "Male",
+  },
+  {
+    date: "2021-08-08",
+    value: 4,
+    type: "Male",
+  },
+  {
+    date: "2021-08-15",
+    value: 6,
+    type: "Male",
+  },
+  {
+    date: "2021-08-22",
+    value: 7,
+    type: "Male",
+  },
+  {
+    date: "2021-08-29",
+    value: 9,
+    type: "Male",
+  },
+];
