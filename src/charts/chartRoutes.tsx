9504b993c85fd76156974bf3cfb8884aed68b0fa
Sam-2019 <samtmartey@gmail.com>
2021-08-27 13:25:53 +0100

    added PieList, ColumnList, new TabList

--- src/charts/chartRoutes.tsx
+++ src/charts/chartRoutes.tsx
@@ -1,19 +1,19 @@
 import { ReactElement } from "react";
 import StackColumn from "./StackColumn";
 import GroupColumn from "./GroupColumn";
+import Pie from "./Pie";
+import {
+  pieTotalData,
+  pieAdultData,
+  pieOmegaData,
+  pieChildrenData,
+} from "./dataset";
 
 interface StringArray {
   [index: string]: ReactElement<any, any>;
 }
 
-export const contentListNoTitle: StringArray = {
-  total: <GroupColumn />,
-  adult: <StackColumn />,
-  omega: <StackColumn />,
-  children: <StackColumn />,
-};
-
-export const tabListNoTitle = [
+export const TabList = [
   {
     key: "total",
     tab: "Total",
@@ -31,3 +31,36 @@ export const tabListNoTitle = [
     tab: "Children",
   },
 ];
+
+export const TabList2 = [
+  {
+    label: "Total",
+    value: "total",
+  },
+  {
+    label: "Adult",
+    value: "adult",
+  },
+  {
+    label: "Omega",
+    value: "omega",
+  },
+  {
+    label: "Children",
+    value: "children",
+  },
+];
+
+export const ColumnList: StringArray = {
+  total: <GroupColumn />,
+  adult: <StackColumn />,
+  omega: <StackColumn />,
+  children: <StackColumn />,
+};
+
+export const PieList: StringArray = {
+  total: <Pie data={pieTotalData} />,
+  adult: <Pie data={pieAdultData} />,
+  omega: <Pie data={pieOmegaData} />,
+  children: <Pie data={pieChildrenData} />,
+};
