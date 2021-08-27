4511b2fa225d19f4e2f5590ffbb3115f1b471861
Sam-2019 <samtmartey@gmail.com>
2021-08-27 13:26:10 +0100

    added Pie chart

---
+++ src/charts/Pie.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Pie } from "@ant-design/charts";
+
+const DemoPie = ({ data }: any) => {
+  var config = {
+    appendPadding: 10,
+    data,
+    angleField: "value",
+    colorField: "type",
+    radius: 1,
+    innerRadius: 0.6,
+    label: {
+      type: "inner",
+      offset: "-50%",
+      content: "{percentage}",
+      style: {
+        textAlign: "center",
+        fontSize: 14,
+      },
+    },
+    interactions: [{ type: "element-selected" }, { type: "element-active" }],
+    statistic: {
+      title: false,
+      content: {
+        style: {
+          whiteSpace: "pre-wrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        },
+        content: "100%",
+      },
+    },
+  };
+  return <Pie {...config} />;
+};
+
+export default DemoPie;
