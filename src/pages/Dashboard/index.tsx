fa0b0995a0ee68da693bba2524e534f8c5e46536
Sam-2019 <samtmartey@gmail.com>
2021-08-28 21:14:29 +0100

    removed unused code

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,21 +8,14 @@ import {
   TabList2,
 } from "../../charts/chartRoutes";
 
-//type ContentKey = "article" | "app" | "project";
-
 const Dashboard = () => {
-  // const [loading, setLoading] = React.useState(true);
   const [noTitleKey, setnoTitleKey] = React.useState<any | null>("total");
   const [value3, setValue3] = React.useState<any | null>("total");
   const [vehicles] = React.useState<any | null>("vehicles");
   const responsive = useBreakpoint();
-  // const onChange = () => {
-  //   setLoading(!loading);
-  // };
 
   const onTabChange = (key: string, type: any): any => {
     console.log(key, type);
-    // setnoTitleKey({ [type]: key });
 
     switch (key) {
       case "total":
@@ -41,17 +34,12 @@ const Dashboard = () => {
     }
   };
 
-  //console.log(noTitleKey);
-  //  console.log(ColumnList);
-
   const onChange = (e: any) => {
     setValue3(e.target.value);
   };
 
   return (
     <>
-      {/* <Switch checked={!loading} onChange={onChange} /> */}
-
       <div className="site-card-wrapper" style={{ marginBottom: 16 }}>
         <Row gutter={[16, responsive.xs ? 16 : 0]}>
           <Col xs={24} sm={24} md={6} lg={6} xl={6}>
@@ -96,9 +84,7 @@ const Dashboard = () => {
       <div className="site-card-wrapper">
         <Row gutter={[16, responsive.xs ? 16 : 0]}>
           <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-            <Card 
-            title="Default size card"
-             style={{ width: "100%" }}>
+            <Card title="Default size card" style={{ width: "100%" }}>
               {ColumnList[vehicles]}
             </Card>
           </Col>
@@ -126,14 +112,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-{
-  /* <Radio.Group onChange={onChange} defaultValue="a">
-{TabList.map((data: any) => (
-  <Radio.Button key={data.key} value={data.key}>
-    {data.tab}
-  </Radio.Button>
-))}
-</Radio.Group>
- */
-}
