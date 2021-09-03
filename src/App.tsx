7cc9699b06d1c0a40bb2692397e78075b213aa24
Sam-2019 <samtmartey@gmail.com>
2021-09-03 23:48:34 +0100

    added @apollo/client, main

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,17 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+import Main from "./Main";
 import "antd/dist/antd.css";
 
-import Layout from "./components/Layout/Layout";
-import { FormsRoute } from "./routes/Forms";
-import { ChapelsRoute } from "./routes/Chapels";
-import { TitheRoute } from "./routes/Tithe";
-import { MemberRoute } from "./routes/Members";
-import { DepartmentRoute } from "./routes/Departments";
-import { DashboardRoute } from "./routes/Dashboard";
-import { NoPageRoute } from "./routes/404";
-import { PledgeRoute } from "./routes/Pledges";
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Layout>
-          <Switch>
-            <Route exact path="/">
-              <DashboardRoute />
-            </Route>
-
-            <Route path="/forms">
-              <FormsRoute />
-            </Route>
-
-            <Route path="/pledges">
-              <PledgeRoute />
-            </Route>
-
-            <Route path="/chapels">
-              <ChapelsRoute />
-            </Route>
-
-            <Route path="/tithe">
-              <TitheRoute />
-            </Route>
-
-            <Route path="/members">
-              <MemberRoute />
-            </Route>
-
-            <Route path="/departments">
-              <DepartmentRoute />
-            </Route>
-
-            <Route>
-              <NoPageRoute />
-            </Route>
-          </Switch>
-        </Layout>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Main />
+    </ApolloProvider>
   );
 }
