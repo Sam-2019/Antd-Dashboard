ebf619eb224e58fe3dbc45f4e9b0e82d70496846
Sam-2019 <samtmartey@gmail.com>
2021-09-06 16:26:19 +0100

    imported checkSlug

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,18 +1,23 @@
 import { useHistory, useParams } from "react-router-dom";
 import { PageHeader } from "antd";
+import { checkSlug } from "../utils/functions";
 
 const GoBack = ({ header }: any) => {
   const history = useHistory();
   let { slug }: any = useParams();
+
+  const result = checkSlug(slug);
 
   return (
     <PageHeader
       className="site-page-header goBack"
       onBack={() => history.goBack()}
       title={
-        <span className="breadcrumb">
-          {slug} {header}
-        </span>
+        <>
+          <span className="breadcrumb">
+            {result ? result : slug} {header}
+          </span>
+        </>
       }
     />
   );
