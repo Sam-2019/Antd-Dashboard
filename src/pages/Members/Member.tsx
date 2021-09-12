0bb0eb19e685dc014a4f01e82c9e904724bfb5ca
Sam-2019 <samtmartey@gmail.com>
2021-09-12 20:13:27 +0100

    added Member page

---
+++ src/pages/Members/Member.tsx
@@ -0,0 +1,45 @@
+import { useQuery } from "@apollo/client";
+import { useParams, useHistory } from "react-router-dom";
+import { GET_MEMBER } from "../../utils/graphqlFunctions/queries";
+
+import MemberItem from "./MemberItem";
+import Spinner from "../../components/Spinner/Spinner";
+import Error from "../../components/Error/Error";
+import { PageHeader } from "antd";
+
+function Member() {
+  let { slug }: any = useParams();
+  const history = useHistory();
+
+  const { loading, error, data } = useQuery(GET_MEMBER, {
+    variables: { memberId: slug },
+  });
+
+  console.log(data);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <>
+      <PageHeader
+        className="site-page-header goBack"
+        onBack={() => history.goBack()}
+        title={
+          <>
+            <span className="breadcrumb">Member Information</span>
+          </>
+        }
+      />
+      
+      <MemberItem dataSource={data.member} />
+    </>
+  );
+}
+
+export default Member;
