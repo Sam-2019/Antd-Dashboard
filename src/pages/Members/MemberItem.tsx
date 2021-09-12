15e85db6834f9358c11a35758bd8d7edcd4f52b2
Sam-2019 <samtmartey@gmail.com>
2021-09-12 20:11:48 +0100

    modified MemberItem page

--- src/pages/Members/MemberItem.tsx
+++ src/pages/Members/MemberItem.tsx
@@ -1,21 +1,86 @@
-import { Descriptions } from "antd";
-import { useParams } from "react-router-dom";
+import { Col, Descriptions, Image, Row, Space, Tag, Typography } from "antd";
+import Title from "antd/lib/typography/Title";
+import { Link } from "react-router-dom";
 
-import GoBack from "../../components/GoBack";
+import { colorSwitch } from "../../utils/functions";
 
-function Member() {
-  let { slug }: any = useParams();
+const { Text } = Typography;
 
+function MemberItem({ dataSource }: any) {
   return (
     <div>
-      <GoBack />
+      <Row>
+        <Col span="4">
+          <Image
+            width={200}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+        </Col>
+        <Col span={20}>
+          {/* <Title level={4}>Member Info</Title> */}
 
-      <Descriptions title="User Info">
-        <Descriptions.Item label="UserName">{slug}</Descriptions.Item>
-        <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-      </Descriptions>
+          <Title level={2}>
+            {dataSource.firstName} {dataSource.lastName}
+          </Title>
+
+          <Descriptions>
+            <Descriptions.Item label="Contact">
+              {dataSource.contact}
+            </Descriptions.Item>
+            <Descriptions.Item label="Location">
+              {dataSource.location}
+            </Descriptions.Item>
+
+            <Descriptions.Item label="Chapel">
+              {dataSource.chapel}
+            </Descriptions.Item>
+
+            <Descriptions.Item label="Residential Address">
+              {dataSource.residentialAddress}
+            </Descriptions.Item>
+          </Descriptions>
+
+          <div>
+            <Title level={5}>Family</Title>
+            <Descriptions>
+              <Descriptions.Item label="Spouse">
+                {dataSource.contact}
+              </Descriptions.Item>
+            </Descriptions>
+
+            <div>
+              <span>
+                Children :
+                {/* {dataSource.nameOfChildren.length === 1 ? "Child" : "Children"} */}
+              </span>
+
+              <Space direction="vertical">
+                {dataSource.department.map((tag: string, index: any) => {
+                  return <Text key={index}>{tag}</Text>;
+                })}
+              </Space>
+            </div>
+          </div>
+
+          <div>
+            <Title level={5}>Departments</Title>
+            {dataSource.department.map((tag: string, index: any) => {
+              let color = colorSwitch(tag);
+              let params = tag.toLocaleLowerCase();
+              return (
+                <Link to={`/departments/${params}`}>
+                  <Tag color={color} key={index}>
+                    {tag}
+                  </Tag>
+                </Link>
+              );
+            })}
+            <div></div>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
 
-export default Member;
+export default MemberItem;
