532e9ac5744d559781b8b76a0c3034b5f92dd52c
Sam-2019 <samtmartey@gmail.com>
2021-09-19 00:35:48 +0100

    renamed Payment/Tyoe

--- src/pages/Forms/Payments/Type.tsx
+++ src/pages/Forms/Payments/Type.tsx
@@ -1,209 +1,22 @@
-import { useState, useEffect } from "react";
-import { useMutation } from "@apollo/client";
-import GoBack from "../../../components/GoBack";
-import { ADD_PAYMENT } from "../../../utils/graphqlFunctions/mutations";
-import { success } from "../../../components/Modal/Modal";
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_MEMBERS } from "../../../utils/graphqlFunctions/queries";
+import TypeItem from "./TypeItem";
+import Spinner from "../../../components/Spinner/Spinner";
+import Error from "../../../components/Error/Error";
 
-import { Transfer, Button, DatePicker, Typography } from "antd";
+function Type() {
+  const { loading, error, data } = useQuery(GET_MEMBERS);
 
-import { useParams } from "react-router-dom";
-
-const { Text } = Typography;
-
-interface MockData {
-  key: string;
-  title: string;
-  description: string;
-  chosen: boolean;
-}
-
-interface MemberData {
-  id: string;
-  firstName: string;
-  lastName: string;
-}
-
-// interface TargetData {
-//   key: string;
-// }
-
-function PaymentType({ dataSource }: any) {
-  let { slug }: any = useParams();
-
-  const [mockData, setMockData] = useState<MemberData[]>([]);
-  const [targetKeys, setTargetKeys] = useState<string[]>([]);
-  const [month, setMonth] = useState<string>("");
-  const [alert, setAlert] = useState({
-    emptytext: false,
-    emptytransfer: false,
-  });
-
-  const [addPaymentPayer] = useMutation(ADD_PAYMENT);
-
-  useEffect(() => {
-    // const getData = data.members;
-    //setMockData(getData);
-    getMock();
-  }, []);
-
-  const getMock = async () => {
-    let trgetKeys: string[] = [];
-    let mckData = [];
-
-    for (let data of dataSource) {
-      console.log(data);
-      // const chosen: Boolean = data.chosen;
-      // const key: string = data.key;
-
-      //   if (chosen) {
-      //     trgetKeys.push(key);
-      //   }
-      mckData.push(data);
-    }
-
-    setMockData(mckData);
-    setTargetKeys(trgetKeys);
-  };
-
-  console.log(mockData);
-  console.log(targetKeys);
-
-  const renderFooter = () => (
-    <Button
-      size="small"
-      style={{ float: "right", margin: 5 }}
-      onClick={dataSource}
-    >
-      Reset
-    </Button>
-  );
-
-  const filterOption = (inputValue: any, option: any) =>
-    option.id.indexOf(inputValue) > -1;
-
-  const handleChange = (targetKeys: any) => {
-    setTargetKeys(targetKeys);
-  };
-
-  const handleSearch = (dir: any, value: any) => {
-    // console.log("search:", dir, value);
-  };
-
-  function onChange(date: any, dateString: any) {
-    setMonth(dateString);
+  if (loading) {
+    return <Spinner />;
   }
 
-  const onFinish = async () => {
-    if (!month && targetKeys.length === 0) {
-      return setAlert((prevState): any => ({
-        ...prevState,
-        emptytext: true,
-        emptytransfer: true,
-      }));
-    }
+  if (error) {
+    return <Error />;
+  }
 
-    if (!month) {
-      return setAlert((prevState): any => ({
-        ...prevState,
-        emptytext: true,
-      }));
-    }
-
-    if (targetKeys.length === 0) {
-      return setAlert((prevState): any => ({
-        ...prevState,
-        emptytransfer: true,
-      }));
-    }
-
-    // console.log(targetKeys);
-
-    // await addProjectOffering({
-    //   variables: {
-    //     addProjectOfferingInput: {
-    //       "members": targetKeys,
-    //       "month": "10-2021",
-    //       "type": "Welfare"
-    //     },
-    //   },
-    // });
-
-    addPaymentPayer({
-      variables: {
-        addPaymentPayerInput: {
-          members: targetKeys,
-          month,
-          type: slug,
-        },
-      },
-    });
-    success("");
-    setAlert((prevState): any => ({
-      ...prevState,
-      emptytext: false,
-      emptytransfer: false,
-    }));
-  };
-
-  return (
-    <>
-      <GoBack />
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginBottom: 10,
-            }}
-          >
-            <DatePicker onChange={onChange} picker="month" />
-            {alert.emptytext ? (
-              <Text type="danger" style={{ margin: 0, paddingLeft: "5px" }}>
-                Required!
-              </Text>
-            ) : null}
-          </div>
-
-          <div>
-            <Transfer
-              dataSource={mockData}
-              showSearch
-              filterOption={filterOption}
-              targetKeys={targetKeys}
-              onChange={handleChange}
-              onSearch={handleSearch}
-              render={(item) => `${item.firstName} ${item.lastName}`}
-              footer={renderFooter}
-              listStyle={{
-                width: 250,
-                height: 500,
-              }}
-              operations={["to right", "to left"]}
-              titles={["Source", "Target"]}
-              oneWay
-            />
-            {alert.emptytransfer ? (
-              <Text type="danger" style={{ margin: 0, paddingLeft: "5px" }}>
-                Required!
-              </Text>
-            ) : null}
-          </div>
-
-          <Button
-            size="small"
-            type="primary"
-            htmlType="submit"
-            style={{ float: "right", margin: 5 }}
-            onClick={onFinish}
-          >
-            Submit
-          </Button>
-        </div>
-      </div>
-    </>
-  );
+  return <TypeItem dataSource={data.members} />;
 }
 
-export default PaymentType;
+export default Type;
--- src/pages/Payments/Type.tsx
+++ src/pages/Payments/Type.tsx
@@ -1,164 +1,49 @@
-import { useState } from "react";
-import { Table, Tag, Space, Button, Input } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
-import Highlighter from "react-highlight-words";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import { GET_MEMBERS } from "../../utils/graphqlFunctions/queries";
+import { GET_PAYMENT } from "../../utils/graphqlFunctions/queries";
 import Spinner from "../../components/Spinner/Spinner";
 import Error from "../../components/Error/Error";
-import { colorSwitch } from "../../utils/functions";
-import GoBack from "../../components/GoBack";
+import { pathSnippetsWithFilter, paymentMonth } from "../../utils/functions";
+import { DatePicker, Space } from "antd";
+import TypeItem from "./TypeItem";
 
-function PaymentType() {
-  const { loading, error, data } = useQuery(GET_MEMBERS);
+function Type() {
+  let location = useLocation();
+  let { slug }: any = useParams();
+  const [year, setYear] = useState("");
+  const [state, setstate] = useState("");
 
-  const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
+  function onChange(date: any, dateString: any) {
+    setYear(dateString);
+  }
+  const paymentType = pathSnippetsWithFilter(location);
+  const month = paymentMonth(slug);
 
-  let searchInput: any;
-
-  const handleSearch = (selectedKeys: any, confirm: any, dataIndex: any) => {
-    confirm();
-
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
-
-  const handleReset = (clearFilters: any) => {
-    clearFilters();
-    setSearchText("");
-  };
-
-  const getColumnSearchProps = (dataIndex: any) => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters,
-    }: any) => (
-      <div style={{ padding: 8 }}>
-        <Input
-          ref={(node: any) => {
-            searchInput = node;
-          }}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          style={{ marginBottom: 8, display: "block" }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({ closeDropdown: false });
-              setSearchText(selectedKeys[0]);
-              setSearchText(dataIndex);
-            }}
-          >
-            Filter
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: any) => (
-      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
-    ),
-    onFilter: (value: any, record: any) =>
-      record[dataIndex]
-        ? record[dataIndex]
-            .toString()
-            .toLowerCase()
-            .includes(value.toLowerCase())
-        : "",
-    onFilterDropdownVisibleChange: (visible: any) => {
-      if (visible) {
-        setTimeout(() => searchInput.select(), 100);
+  useEffect(() => {
+    const check = () => {
+      if (!year) {
+        return;
+      } else {
+        setstate(`${year}-${month}`);
       }
+    };
+
+    check();
+
+    return () => {
+      check();
+    };
+  }, [month, year]);
+
+  console.log(state);
+
+  const { loading, error, data } = useQuery(GET_PAYMENT, {
+    variables: {
+      paymentMonth: state,
+      paymentType: paymentType[1],
     },
-    render: (text: any, record: any): any =>
-      searchedColumn === dataIndex ? (
-        <Space size="middle">
-          <Link to={`/members/${record.firstName} ${record.lastName}`}>
-            <Highlighter
-              highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
-              searchWords={[searchText]}
-              autoEscape
-              textToHighlight={
-                text ? ` ${record.firstName} ${record.lastName}` : ""
-              }
-            />
-          </Link>
-        </Space>
-      ) : (
-        <Space size="middle">
-          <Link to={`/members/${record.firstName} ${record.lastName}`}>
-            {record.firstName} {record.lastName}
-          </Link>
-        </Space>
-      ),
   });
-
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "firstName",
-      key: "firstName",
-      ...getColumnSearchProps("firstName"),
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
-    },
-    {
-      title: "Email",
-      dataIndex: "emailAddress",
-      key: "emailAddress",
-    },
-    {
-      title: "Department",
-      dataIndex: "department",
-      key: "department",
-      width: "60%",
-      render: (department: string[]) => (
-        <>
-          {department.map((tag: string, index: any): any => {
-            let color = colorSwitch(tag);
-            let params = tag.toLocaleLowerCase();
-
-            return (
-              <Link to={`/departments/${params}`}>
-                <Tag color={color} key={index}>
-                  {tag}
-                </Tag>
-              </Link>
-            );
-          })}
-        </>
-      ),
-    },
-  ];
 
   if (loading) {
     return <Spinner />;
@@ -170,10 +55,13 @@ function PaymentType() {
 
   return (
     <>
-      <GoBack />
-      <Table rowKey="id" columns={columns} dataSource={data.members} />;
+      <Space direction="vertical">
+        <DatePicker onChange={onChange} picker="year" />
+      </Space>
+
+      <TypeItem dataSource={data.payment} />
     </>
   );
 }
 
-export default PaymentType;
+export default Type;
