16c1816793d5ed49d39952da4a9da3b4f44248e4
Sam-2019 <samtmartey@gmail.com>
2021-09-26 01:24:48 +0100

    updated chartRoute

---
+++ src/charts/chartRoute.tsx
@@ -0,0 +1,66 @@
+import { ReactElement } from "react";
+
+import {
+  GET_GROUP_STATS,
+  GET_SUNDAY_STATS,
+} from "../utils/graphqlFunctions/queries";
+import GroupChart from "./GroupColumn";
+import Pie from "./Pie";
+import StackColumn from "./StackColumn";
+
+interface StringArray {
+  [index: string]: ReactElement<any, any>;
+}
+
+export const TabList = [
+  {
+    key: "total",
+    tab: "Total",
+  },
+  {
+    key: "adult",
+    tab: "Adult",
+  },
+  {
+    key: "omega",
+    tab: "Omega",
+  },
+  {
+    key: "children",
+    tab: "Children",
+  },
+];
+
+export const TabList2 = [
+  {
+    label: "Total",
+    value: "total",
+  },
+  {
+    label: "Adult",
+    value: "adult",
+  },
+  {
+    label: "Omega",
+    value: "omega",
+  },
+  {
+    label: "Children",
+    value: "children",
+  },
+];
+
+export const ColumnList: StringArray = {
+  total: <GroupChart info={GET_SUNDAY_STATS} type="sundayTotal" />,
+  adult: <StackColumn info={GET_GROUP_STATS} type="adult" />,
+  omega: <StackColumn info={GET_GROUP_STATS} type="omega" />,
+  children: <StackColumn info={GET_GROUP_STATS} type="children" />,
+  vehicles: <GroupChart info={GET_SUNDAY_STATS} type="vehicles" />,
+};
+
+export const PieList: StringArray = {
+  total: <Pie />,
+  adult: <Pie />,
+  omega: <Pie />,
+  children: <Pie />,
+};
