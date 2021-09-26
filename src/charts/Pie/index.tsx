87aa4f3b380bbb037392c646786117a342ef1c1b
Sam-2019 <samtmartey@gmail.com>
2021-09-26 03:04:10 +0100

    added loading & error handlers

--- src/charts/Pie/index.tsx
+++ src/charts/Pie/index.tsx
@@ -4,7 +4,16 @@ import Error from "../../components/Error/Error";
 import Pie from "./Pie";
 
 function PieChart({ info, type }: any) {
-  return <Pie />;
+  const { loading, error, data } = useQuery(info);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+  return <Pie data={data.countGender} />;
 }
 
 export default PieChart;
