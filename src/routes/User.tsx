3a57427e5652f6ad5e8cf7f7c3785010f2174d1d
Sam-2019 <samtmartey@gmail.com>
2022-04-17 00:41:43 +0100

    added VerifyEmailRoute, ResetPasswordRoute

--- src/routes/User.tsx
+++ src/routes/User.tsx
@@ -1,5 +1,7 @@
 import Login from "../pages/User/Login";
 import Signup from "../pages/User/Signup";
+import VerifyEmail from "../pages/User/Verify Email";
+import ResetPassword from "../pages/User/Reset Password";
 
 export function LoginRoute() {
   return <Login />;
@@ -9,5 +11,15 @@ export function SignupRoute() {
   return <Signup />;
 }
 
+export function VerifyEmailRoute() {
+  return <VerifyEmail />;
+}
+
+export function ResetPasswordRoute() {
+  return <ResetPassword />;
+}
+
 export { LoginRoute as default } from "./User";
 export { SignupRoute as Signup } from "./User";
+export { VerifyEmailRoute as VerifyEmail } from "./User";
+export { ResetPasswordRoute as ResetPassword } from "./User";
