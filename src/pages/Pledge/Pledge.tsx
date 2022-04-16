009367f8b45e8115d399e77b4dcd02183f1e8d1e
Sam-2019 <samtmartey@gmail.com>
2022-04-16 09:44:53 +0100

    added Pledge page

---
+++ src/pages/Pledge/Pledge.tsx
@@ -0,0 +1,118 @@
+import { useQuery } from "@apollo/client";
+import { useParams, useHistory } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { GET_PLEDGEE, GET_PLEDGES } from "../../utils/graphqlFunctions/queries";
+import { UPDATE_PLEDGE_STATUS } from "../../utils/graphqlFunctions/mutations";
+import { Success, Error as ErrorMessage } from "../../components/Modal/Modal";
+import PledgeItem from "./PledgeItem";
+import Spinner from "../../components/Spinner/Spinner";
+import Error from "../../components/Error/Error";
+import { Button, PageHeader } from "antd";
+import PledgeEdit from "../Forms/Pledge/Edit";
+import { Edit } from "../../components/Modal/Modal";
+import { useState } from "react";
+
+function Pledge() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  let { slug }: any = useParams();
+  const history = useHistory();
+
+  const { loading, error, data } = useQuery(GET_PLEDGEE, {
+    variables: { pledgeId: slug },
+  });
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const [updatePledgeStatus] = useMutation(UPDATE_PLEDGE_STATUS, {
+    refetchQueries: [
+      {
+        query: GET_PLEDGEE,
+        variables: {
+          pledgeId: slug,
+        },
+      },
+      // {
+      //   query: GET_PLEDGES,
+      // },
+    ],
+  });
+
+  const updateStatus = async () => {
+    const values = {
+      status: "REDEEMED",
+    };
+
+    // console.log(slug, values);
+    const data = await updatePledgeStatus({
+      variables: {
+        pledgeId: slug,
+        pledgeInput: {
+          ...values,
+        },
+      },
+    });
+
+    if (!data) {
+      return ErrorMessage("Updated failed");
+    }
+
+    Success("Pledge status updated");
+  };
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <>
+      <PageHeader
+        className="site-page-header goBack"
+        onBack={() => history.goBack()}
+        title={
+          <>
+            <span className="breadcrumb">Pledgee</span>
+          </>
+        }
+        extra={[
+          <Button key="1" type="default" onClick={showModal}>
+            Edit
+          </Button>,
+          <Button key="1" type="primary" onClick={updateStatus}>
+            Paid
+          </Button>,
+        ]}
+      />
+
+      <PledgeItem dataSource={data.pledge} />
+
+      <Edit
+        isModalVisible={isModalVisible}
+        handleOk={handleOk}
+        handleCancel={handleCancel}
+        children={
+          <PledgeEdit
+            handleCancel={handleCancel}
+            data={data.pledge}
+            slug={slug}
+          />
+        }
+      />
+    </>
+  );
+}
+
+export default Pledge;
