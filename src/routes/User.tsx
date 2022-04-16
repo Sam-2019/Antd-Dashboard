a289900764b24dc51eaaaff012bf1f6cdeb16219
Sam-2019 <samtmartey@gmail.com>
2022-04-16 18:50:13 +0100

    added user auth pages

--- src/routes/User.tsx
+++ src/routes/User.tsx
@@ -1,26 +1,12 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
 import Login from "../pages/User/Login";
 import Signup from "../pages/User/Signup";
 
 export function LoginRoute() {
-  return (
-    <Switch>
-      <Route path={`login`}>
-        <Login />
-      </Route>
-    </Switch>
-  );
+  return <Login />;
 }
 
 export function SignupRoute() {
-  return (
-    <Switch>
-      <Route path={`signup`}>
-        <Signup />
-      </Route>
-    </Switch>
-  );
+  return <Signup />;
 }
 
 export { LoginRoute as default } from "./User";
