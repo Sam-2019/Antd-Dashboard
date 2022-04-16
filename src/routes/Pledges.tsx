a303c61b3a923e5e0e7979c715f104b423b7ef22
Sam-2019 <samtmartey@gmail.com>
2022-04-16 09:44:31 +0100

    added Pledge

--- src/routes/Pledges.tsx
+++ src/routes/Pledges.tsx
@@ -1,10 +1,15 @@
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import Pledges from "../pages/Pledge";
+import Pledge from "../pages/Pledge/Pledge";
 
 export function PledgeRoute() {
   let { path } = useRouteMatch();
   return (
     <Switch>
+      <Route path={`${path}/:slug`}>
+        <Pledge />
+      </Route>
+
       <Route path={`${path}`}>
         <Pledges />
       </Route>
