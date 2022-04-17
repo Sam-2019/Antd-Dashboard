23500787f17295072b37c16fce1f93eef52cc0af
Sam-2019 <samtmartey@gmail.com>
2022-04-17 08:04:52 +0100

    added styles

---
+++ src/utils/styles.tsx
@@ -0,0 +1,21 @@
+export const contentStyles = {
+  padding: "50px 0px 0px 0px",
+  height: "92vh",
+  display: "flex",
+  justifyContent: "center",
+};
+
+export const formStyles = {
+  mobile: "300px",
+  nonMobile: "80%",
+};
+
+export const forgotPasswordStyles = {
+  display: "flex",
+  justifyContent: "right",
+  padding: "0 0 15px 0",
+};
+
+export const spaceStyles = {
+  width: "100%",
+};
