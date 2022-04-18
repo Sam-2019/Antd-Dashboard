3a3c8b132cd94c7cf7816f72349ce13361c61cd3
Sam-2019 <samtmartey@gmail.com>
2022-04-18 23:45:09 +0100

    imported Profile & Settings pages

--- src/routes/User.tsx
+++ src/routes/User.tsx
@@ -2,6 +2,8 @@ import Login from "../pages/User/Login";
 import Signup from "../pages/User/Signup";
 import VerifyEmail from "../pages/User/Verify Email";
 import ResetPassword from "../pages/User/Reset Password";
+import Profile from "../pages/User/Profile";
+import Settings from "../pages/User/Settings";
 
 export function LoginRoute() {
   return <Login />;
@@ -19,7 +21,17 @@ export function ResetPasswordRoute() {
   return <ResetPassword />;
 }
 
+export function ProfileRoute() {
+  return <Profile />;
+}
+
+export function SettingsRoute() {
+  return <Settings />;
+}
+
 export { LoginRoute as default } from "./User";
 export { SignupRoute as Signup } from "./User";
 export { VerifyEmailRoute as VerifyEmail } from "./User";
 export { ResetPasswordRoute as ResetPassword } from "./User";
+export { ProfileRoute as Profile } from "./User";
+export { SettingsRoute as Settings } from "./User";
