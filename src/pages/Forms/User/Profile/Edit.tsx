5315fb2d336b1e4bcdd6444f9da7543aa9e0fead
Sam-2019 <samtmartey@gmail.com>
2022-05-11 07:35:06 +0100

    added profile edit

---
+++ src/pages/Forms/User/Profile/Edit.tsx
@@ -0,0 +1,107 @@
+import { Input, Form, Button, Radio, Space, DatePicker } from "antd";
+import { useMutation } from "@apollo/client";
+import { UPDATE_USER_DETAILS } from "../../../../utils/graphqlFunctions/mutations";
+import { Success, Error } from "../../../../components/Modal/Modal";
+import { useParams } from "react-router-dom";
+import { USER_DEATILS } from "../../../../utils/graphqlFunctions/queries";
+import { inputStyles } from "../../../../utils/styles";
+
+function Profile({ handleCancel, data }: any) {
+  const [form] = Form.useForm();
+  let { slug }: any = useParams();
+
+  const [updateVisitor] = useMutation(UPDATE_USER_DETAILS, {
+    refetchQueries: [{ query: USER_DEATILS, variables: { visitorId: slug } }],
+  });
+
+  const onFinish = async (fieldsValue: any) => {
+    const data = await updateVisitor({
+      variables: {
+        updateVisitorId: slug,
+        updateVisitorInput: {
+          ...fieldsValue,
+        },
+      },
+    });
+
+    if (!data) {
+      return Error("Update failed");
+    }
+
+    form.resetFields();
+
+    Success("Visitor updated");
+
+    handleCancel();
+  };
+
+  return (
+    <div>
+      <Form
+        form={form}
+        name="visitor"
+        onFinish={onFinish}
+        labelCol={{ span: 10 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{
+          firstName: data.firstName,
+          lastName: data.lastName,
+          gender: data.gender,
+          contact: data.contact,
+          email: data.email,
+          address: data.address,
+          dob: data.dob,
+        }}
+      >
+        <Form.Item name="firstName" label="First Name">
+          <Input style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item name="lastName" label="Last Name">
+          <Input style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item name="gender" label="Gender">
+          <Radio.Group>
+            <Radio.Button value="male">Male</Radio.Button>
+            <Radio.Button value="female">Female</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item name="contact" label="Contact">
+          <Input style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item name="email" label="Email">
+          <Input style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item name="address" label="Address">
+          <Input style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item name="dob" label="Date of Birth">
+          <DatePicker style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            xs: { span: 24, offset: 0 },
+            sm: { span: 16, offset: 10 },
+          }}
+        >
+          <Space>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <Button htmlType="button" onClick={handleCancel}>
+              Cancel
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+}
+
+export default Profile;
