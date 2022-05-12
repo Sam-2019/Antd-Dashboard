39eae8e1381c8f7f970b81d56d1f1879240e5f06
Sam-2019 <samtmartey@gmail.com>
2022-05-12 07:24:17 +0100

    updated settings route

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route, useRouteMatch } from "react-router-dom";
-import Settings from "../pages/User/Settings";
+import Settings from "../pages/User/Settings/index";
 
 export function SettingsRoute() {
   let { path } = useRouteMatch();
