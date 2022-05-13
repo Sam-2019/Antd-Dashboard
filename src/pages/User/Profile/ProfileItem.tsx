4a751fce4cd2b306682dd162cb7218ad1dc60476
Sam-2019 <samtmartey@gmail.com>
2022-05-13 23:29:44 +0100

    added userData

--- src/pages/User/Profile/ProfileItem.tsx
+++ src/pages/User/Profile/ProfileItem.tsx
@@ -3,7 +3,7 @@ import ImageUpload from "../../../components/ImageUpload";
 
 export default function ProfileItem({ dataSource }: any) {
   const userImage = false;
-  const imageWidth = '100%'
+  const imageWidth = "100%";
   return (
     <div className="site-card-wrapper">
       <Row gutter={16}>
@@ -28,7 +28,7 @@ export default function ProfileItem({ dataSource }: any) {
                       alt="example"
                       src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
                       width={imageWidth}
-                      style={{marginBottom: '10px'}}
+                      style={{ marginBottom: "10px" }}
                     />
                     <ImageUpload />
                   </div>
@@ -36,9 +36,7 @@ export default function ProfileItem({ dataSource }: any) {
               </>
             }
             style={{ padding: 15 }}
-          >
-       
-          </Card>
+          ></Card>
         </Col>
 
         <Col span={18}>
@@ -46,24 +44,41 @@ export default function ProfileItem({ dataSource }: any) {
             <Col span={24}>
               <Card bordered={false}>
                 <Descriptions title="About" size="default">
-                  <Descriptions.Item label="First Name">
-                    Cloud Database
+                  <Descriptions.Item label="Name">
+                    {!dataSource.firstName ? (
+                      "N/A"
+                    ) : (
+                      <>
+                        {dataSource.firstName} {dataSource.lastName}
+                      </>
+                    )}
                   </Descriptions.Item>
-                  <Descriptions.Item label="Last Name">
-                    Prepaid
+                  <Descriptions.Item label="Gender">
+                    {!dataSource.gender ? "N/A" : <> {dataSource.gender} </>}
                   </Descriptions.Item>
-                  <Descriptions.Item label="Gender">18:00:00</Descriptions.Item>
-                  <Descriptions.Item label="Contact">$80.00</Descriptions.Item>
-                  <Descriptions.Item label="Email">$20.00</Descriptions.Item>
-                  <Descriptions.Item label="Birthday">$60.00</Descriptions.Item>
-                  <Descriptions.Item label="Address">$60.00</Descriptions.Item>
+                  <Descriptions.Item label="Contact">
+                    {!dataSource.contact ? "N/A" : <> {dataSource.contact} </>}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Email">
+                    {!dataSource.emailAddress ? (
+                      "N/A"
+                    ) : (
+                      <> {dataSource.emailAddress} </>
+                    )}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Birthday">
+                    {!dataSource.bod ? "N/A" : <> {dataSource.dob} </>}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Address">
+                    {!dataSource.homeAddress ? (
+                      "N/A"
+                    ) : (
+                      <> {dataSource.homeAddress} </>
+                    )}
+                  </Descriptions.Item>
                 </Descriptions>
               </Card>
             </Col>
-
-            {/* <Col span={24}>
-                <Card bordered={false}>Card content</Card>
-              </Col> */}
           </Row>
         </Col>
       </Row>
