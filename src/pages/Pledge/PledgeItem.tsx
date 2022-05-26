09df7c958131bbcb60d997a9f7dfbf554f4afae6
Sam-2019 <samtmartey@gmail.com>
2022-05-26 07:13:55 +0100

    updated view

--- src/pages/Pledge/PledgeItem.tsx
+++ src/pages/Pledge/PledgeItem.tsx
@@ -1,46 +1,102 @@
-import { Col, Descriptions, Row, Tag } from "antd";
+import { Card, Col, Descriptions, Row, Tag } from "antd";
 import Title from "antd/lib/typography/Title";
 
 export default function PledgeItem({ dataSource }: any) {
   return (
-    <div>
-      <Row>
-        <Col span={19}>
-          <Title level={2}>
-            {dataSource.firstName} {dataSource.lastName}
-          </Title>
+    <div className="site-card-wrapper">
+      <Row gutter={16}>
+        {/* <Col span={6}>
+          <Card
+            type="inner"
+            hoverable
+            bordered={false}
+            cover={
+              <>
+                {dataSource.imageURL ? (
+                  <>
+                    <Image
+                      alt="example"
+                      src={dataSource.imageURL}
+                      width={imageWidth}
+                      style={{ marginBottom }}
+                      fallback={fallback}
+                    />
+                  </>
+                ) : (
+                  <div>
+                    <img
+                      alt="example"
+                      src={dummyImage}
+                      width={imageWidth}
+                      style={{ marginBottom }}
+                    />
 
-          <Descriptions>
-            <Descriptions.Item label="Contact">
-              {dataSource.contact}
-            </Descriptions.Item>
+                    <ImageUpload
+                      id={dataSource.id}
+                      type="member"
+                      query={GET_MEMBER}
+                    />
+                  </div>
+                )}
+              </>
+            }
+            style={{ padding: 15 }}
+          ></Card>
+        </Col> */}
 
-            <Descriptions.Item label="Email">
-              {dataSource.emailAddress}
-            </Descriptions.Item>
+        <Col span={24}>
+          <Row gutter={[16, 16]}>
+            <Col span={24}>
+              <Card bordered={false}>
+                <Title level={2}>
+                  {dataSource.firstName} {dataSource.lastName}
+                </Title>
 
-            <Descriptions.Item label="Programme">
-              {dataSource.programme}
-            </Descriptions.Item>
-          </Descriptions>
+                <Descriptions>
+                  <Descriptions.Item label="Contact">
+                    {dataSource.contact}
+                  </Descriptions.Item>
 
-          <div>
-            <Descriptions>
-              <Descriptions.Item label="Amount">
-                <>GHc {dataSource.amount} </>
-              </Descriptions.Item>
-            </Descriptions>
-          </div>
+                  <Descriptions.Item label="Email">
+                    {dataSource.emailAddress}
+                  </Descriptions.Item>
+                </Descriptions>
 
-          <div>
-            <Descriptions>
-              <Descriptions.Item label="Status">
-                <Tag color={dataSource.status === "PENDING" ? "red" : "green"}>
-                  {dataSource.status}
-                </Tag>
-              </Descriptions.Item>
-            </Descriptions>
-          </div>
+                <div>
+                  <Title level={5}>Programme</Title>
+                  <Descriptions>
+                    <Descriptions.Item label="Programme">
+                      {dataSource.programme}
+                    </Descriptions.Item>
+                  </Descriptions>
+                </div>
+
+                <div>
+                  <Title level={5}>Amount</Title>
+                  <Descriptions>
+                    <Descriptions.Item>
+                      <>GHc {dataSource.amount} </>
+                    </Descriptions.Item>
+                  </Descriptions>
+                </div>
+
+                <div>
+                  <Title level={5}>Status</Title>
+                  <Descriptions>
+                    <Descriptions.Item>
+                      <Tag
+                        color={
+                          dataSource.status === "PENDING" ? "red" : "green"
+                        }
+                      >
+                        {dataSource.status}
+                      </Tag>
+                    </Descriptions.Item>
+                  </Descriptions>
+                </div>
+              </Card>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </div>
