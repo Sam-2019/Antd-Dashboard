06846a4f2685a82ad27690ef974490f2cc95288b
Sam-2019 <samtmartey@gmail.com>
2022-06-29 22:48:00 +0100

    updated apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { ConfigProvider } from "antd";
 import Main from "./Main";
 import "antd/dist/antd.css";
 import { onlinehost, localhost, env } from "./utils/config";
 
-const cache = new InMemoryCache();
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: env === "production" ? onlinehost : localhost,
-  cache,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <Main />
+      <ConfigProvider>
+        <Main />
+      </ConfigProvider>
     </ApolloProvider>
   );
 }
